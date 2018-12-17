72e9809e8c5c06c1d980d61423840b236246fa92
Juan Ramos <juan-rb01@hotmail.com>
2018-12-17 15:05:03 -0600

    env var

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended : true}))
 
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.listen(port, ()=>{
   console.log('server started...')
